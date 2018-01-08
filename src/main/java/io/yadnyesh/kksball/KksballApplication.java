f6f934e2dc15e1368a4050c5e0e67bf1fae1c96d
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-01-08 20:25:25 +0530

    First Commit with JPA H2 HSQLDB ApacheDerby Web

---
+++ src/main/java/io/yadnyesh/kksball/KksballApplication.java
@@ -0,0 +1,12 @@
+package io.yadnyesh.kksball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KksballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KksballApplication.class, args);
+	}
+}
