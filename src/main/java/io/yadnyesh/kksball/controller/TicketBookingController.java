48f2bd71fcda5b7fdc9feac6c8d9e3d2e6c5a361
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-01-13 08:30:20 +0530

    Added save method to Service

--- src/main/java/io/yadnyesh/kksball/controller/TicketBookingController.java
+++ src/main/java/io/yadnyesh/kksball/controller/TicketBookingController.java
@@ -1,8 +1,17 @@
 package io.yadnyesh.kksball.controller;
 
+import io.yadnyesh.kksball.entity.Ticket;
+import io.yadnyesh.kksball.repository.TicketBookingRepository;
+import io.yadnyesh.kksball.service.TicketBookingService;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TicketBookingController {
+	
+	private TicketBookingService ticketBookingService;
+	
+	public Ticket createTicket(Ticket ticketRequest){
+		return ticketBookingService.createTicket(ticketRequest);
+	}
 
 }
---
+++ src/main/java/io/yadnyesh/kksball/service/TicketBookingService.java
@@ -0,0 +1,18 @@
+package io.yadnyesh.kksball.service;
+
+import io.yadnyesh.kksball.entity.Ticket;
+import io.yadnyesh.kksball.repository.TicketBookingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TicketBookingService {
+
+	@Autowired
+	TicketBookingRepository ticketBookingRepository;
+	
+	
+	public Ticket createTicket(Ticket ticket){
+		return ticketBookingRepository.save(ticket);
+	}
+}
