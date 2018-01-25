68b6ff5e9af9b39238e1865137410e13c8455146
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-01-25 19:43:59 +0530

    Removing Unnecessary code

--- src/main/java/io/yadnyesh/kksball/KksballApplication.java
+++ src/main/java/io/yadnyesh/kksball/KksballApplication.java
@@ -1,15 +1,8 @@
 package io.yadnyesh.kksball;
 
-import io.yadnyesh.kksball.entity.Ticket;
-import io.yadnyesh.kksball.service.TicketBookingService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-
-import javax.sql.DataSource;
-import java.util.Date;
 
 @SpringBootApplication
 public class KksballApplication {
