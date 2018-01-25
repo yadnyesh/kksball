4c2574fa34c5b91283d0d4f88dedd0e5bc402597
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-01-25 19:54:35 +0530

    Added the repository class

---
+++ src/main/java/io/yadnyesh/kksball/repository/PersonManagementRepository.java
@@ -0,0 +1,8 @@
+package io.yadnyesh.kksball.repository;
+
+import io.yadnyesh.kksball.entity.Person;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PersonManagementRepository extends CrudRepository<Person, Integer> {
+
+}
