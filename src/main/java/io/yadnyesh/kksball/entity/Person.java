ef60a7667f49cca9c88bf685c2afdbad79984a7d
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-01-25 19:47:27 +0530

    Added the attributes for Person

--- src/main/java/io/yadnyesh/kksball/entity/Person.java
+++ src/main/java/io/yadnyesh/kksball/entity/Person.java
@@ -1,10 +1,27 @@
 package io.yadnyesh.kksball.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name="person")
 public class Person {
-
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="person_id")
+	private int id;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="create_date")
+	private Date createdDate;
+	
 }
