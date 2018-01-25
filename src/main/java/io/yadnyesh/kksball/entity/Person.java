21a6a7203565bb60e9b60a679a05092c75130563
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-01-25 19:47:47 +0530

    Added Getters and Setters

--- src/main/java/io/yadnyesh/kksball/entity/Person.java
+++ src/main/java/io/yadnyesh/kksball/entity/Person.java
@@ -24,4 +24,43 @@ public class Person {
 	@Column(name="create_date")
 	private Date createdDate;
 	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+	
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
 }
