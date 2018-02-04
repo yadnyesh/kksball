6da7a344baf3112bd8ee65f71e5d8d768b3766f9
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-02-04 18:45:52 +0530

    Changes to hibernate properties

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,7 @@
 insert into ticket (booking_date, dest_station, email, passenger_name, source_station, ticket_id) values ('12/12/2017', 'Patna', 'dravid@cricket.in','Rahul','Chennai',2 );
+INSERT INTO public.ticket (ticket_id, booking_date, dest_station, email, passenger_name, source_station) VALUES (2, '2017-12-12 00:00:00.000000', 'Ranchi', 'dravid@cricket.in', 'Rahul', 'Chennai');
+INSERT INTO public.ticket (ticket_id, booking_date, dest_station, email, passenger_name, source_station) VALUES (1, '2017-12-12 00:00:00.000000', 'Test', 'dravid2@cricket.in', 'Rahul2', 'Chennai');
+INSERT INTO public.ticket (ticket_id, booking_date, dest_station, email, passenger_name, source_station) VALUES (3, '2017-12-12 00:00:00.000000', 'Match', 'dravid3@cricket.in', 'Rahul3', 'Chennai');
+INSERT INTO public.ticket (ticket_id, booking_date, dest_station, email, passenger_name, source_station) VALUES (4, '2017-12-12 00:00:00.000000', 'One', 'dravid4@cricket.in', 'Rahul4', 'Chennai');
+INSERT INTO public.ticket (ticket_id, booking_date, dest_station, email, passenger_name, source_station) VALUES (5, '2017-12-12 00:00:00.000000', 'Day', 'dravid5@cricket.in', 'Rahul5', 'Chennai');
+INSERT INTO public.ticket (ticket_id, booking_date, dest_station, email, passenger_name, source_station) VALUES (6, '2017-12-12 00:00:00.000000', 'T20', 'dravid6@cricket.in', 'Rahul6', 'Chennai');
