3e39a8e17684f9800c9a611daee83446948926ca
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-02-05 19:23:53 +0530

    Trying to add records from CommandLineRunner

--- src/main/java/io/yadnyesh/kksball/KksballApplication.java
+++ src/main/java/io/yadnyesh/kksball/KksballApplication.java
@@ -1,12 +1,25 @@
 package io.yadnyesh.kksball;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import io.yadnyesh.kksball.repository.PersonManagementRepository;
 
 @SpringBootApplication
-public class KksballApplication {
+public class KksballApplication implements CommandLineRunner {
+	
+	@Autowired
+	private PersonManagementRepository personManagementRepository;
+	
 	public static void main(String[] args) {
+		
 		ConfigurableApplicationContext applicationContext = SpringApplication.run(KksballApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... strings) throws Exception {
+	
 	}
 }
