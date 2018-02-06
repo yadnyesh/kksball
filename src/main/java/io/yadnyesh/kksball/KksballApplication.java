46a29abb90bbe6fe9472b162335e131b97070c84
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-02-06 11:41:51 +0530

    Currently not working

--- src/main/java/io/yadnyesh/kksball/KksballApplication.java
+++ src/main/java/io/yadnyesh/kksball/KksballApplication.java
@@ -1,11 +1,14 @@
 package io.yadnyesh.kksball;
 
+import io.yadnyesh.kksball.entity.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import io.yadnyesh.kksball.repository.PersonManagementRepository;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class KksballApplication implements CommandLineRunner {
@@ -20,6 +23,11 @@ public class KksballApplication implements CommandLineRunner {
 	
 	@Override
 	public void run(String... strings) throws Exception {
+		createPerson();
+	}
 	
+	private void createPerson() {
+		Person person1 = new Person("Sean", "Murphy", "sean.m2018@gmail.com", new Date());
+		System.out.println(personManagementRepository.save(person1));
 	}
 }
