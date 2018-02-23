656f53ca31effca295181d959415ae7ebedd9a09
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-02-23 20:00:19 +0530

    Added create Person

--- src/main/java/io/yadnyesh/kksball/service/PersonManagementService.java
+++ src/main/java/io/yadnyesh/kksball/service/PersonManagementService.java
@@ -1,8 +1,18 @@
 package io.yadnyesh.kksball.service;
 
+import io.yadnyesh.kksball.entity.Person;
+import io.yadnyesh.kksball.repository.PersonManagementRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PersonManagementService {
 
+	@Autowired
+	PersonManagementRepository personManagementRepository;
+	
+	
+	public Person createPerson(Person person){
+		return personManagementRepository.save(person);
+	}
 }
