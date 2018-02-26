6cfdd1d30dfc1b5a4e05d4b28f7779756f403466
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-02-26 19:02:37 +0530

    Adding @Query annotation with HQL

--- src/main/java/io/yadnyesh/kksball/repository/PersonManagementRepository.java
+++ src/main/java/io/yadnyesh/kksball/repository/PersonManagementRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.scheduling.annotation.Async;
 
 import java.util.List;
@@ -14,6 +15,6 @@ import java.util.function.Predicate;
 
 public interface PersonManagementRepository extends CrudRepository<Person, Integer> {
 	
-	@Query
-	List<Person> findByLastNameOrFirstName(String lastName, String firstName);
+	@Query("SELECT Person p WHERE p.firstName =:firstName OR p.lastName=:lastName")
+	List<Person> findByLastNameOrFirstName(@Param("lastName") String lastName, @Param("firstName") String firstName);
 }
