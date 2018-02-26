11875986d567de53bffb3b07187f90bd13376333
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-02-26 18:48:26 +0530

    Async - Annotation

--- src/main/java/io/yadnyesh/kksball/repository/PersonManagementRepository.java
+++ src/main/java/io/yadnyesh/kksball/repository/PersonManagementRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.scheduling.annotation.Async;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -13,5 +14,6 @@ import java.util.function.Predicate;
 
 public interface PersonManagementRepository extends CrudRepository<Person, Integer> {
 	
+	@Async
 	CompletableFuture<Person> findByEmail(String email);
 }
