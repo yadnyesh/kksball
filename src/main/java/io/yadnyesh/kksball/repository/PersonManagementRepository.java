31b1eac4c9811587d6aa6d84ce030c53b863cfbc
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-03-03 16:32:04 +0530

    Code cleanup before Async implementation

--- src/main/java/io/yadnyesh/kksball/repository/PersonManagementRepository.java
+++ src/main/java/io/yadnyesh/kksball/repository/PersonManagementRepository.java
@@ -1,13 +1,8 @@
 package io.yadnyesh.kksball.repository;
 
 import io.yadnyesh.kksball.entity.Person;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 
 public interface PersonManagementRepository extends CrudRepository<Person, Integer> {
 
