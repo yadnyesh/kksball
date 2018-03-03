261475995ec02fcd4519f677c8537b4ae7ddbbf2
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-03-03 16:03:38 +0530

    Removed unnecessary code

--- src/main/java/io/yadnyesh/kksball/KksballApplication.java
+++ src/main/java/io/yadnyesh/kksball/KksballApplication.java
@@ -1,6 +1,5 @@
 package io.yadnyesh.kksball;
 
-import com.sun.xml.internal.ws.util.CompletedFuture;
 import io.yadnyesh.kksball.entity.Person;
 import io.yadnyesh.kksball.service.PersonManagementService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,16 +7,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import io.yadnyesh.kksball.repository.PersonManagementRepository;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
-import org.springframework.scheduling.annotation.EnableAsync;
 
-import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.TimeUnit;
+
 
 @SpringBootApplication
 public class KksballApplication implements CommandLineRunner {
