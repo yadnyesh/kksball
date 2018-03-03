528ff9f1dcc489b397a3b98eb00d576676dc1bb2
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-03-03 16:34:17 +0530

    Changes to Main application class

--- src/main/java/io/yadnyesh/kksball/KksballApplication.java
+++ src/main/java/io/yadnyesh/kksball/KksballApplication.java
@@ -1,5 +1,6 @@
 package io.yadnyesh.kksball;
 
+import com.sun.xml.internal.ws.util.CompletedFuture;
 import io.yadnyesh.kksball.entity.Person;
 import io.yadnyesh.kksball.service.PersonManagementService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +13,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
 import java.util.List;
-
+import java.util.concurrent.CompletableFuture;
 
 @SpringBootApplication
 public class KksballApplication implements CommandLineRunner {
@@ -27,5 +28,6 @@ public class KksballApplication implements CommandLineRunner {
 	
 	@Override
 	public void run(String... strings) throws Exception {
+		CompletableFuture<Person> personCompletableFuture = personManagementService.findByEmail("sean5.m2018@gmail.com");
 	}
 }
