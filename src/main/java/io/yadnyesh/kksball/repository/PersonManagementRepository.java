e658ca22f62c4992c763c326f225f8d216131f4e
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-03-03 16:35:35 +0530

    Declared ASYNC repository method

--- src/main/java/io/yadnyesh/kksball/repository/PersonManagementRepository.java
+++ src/main/java/io/yadnyesh/kksball/repository/PersonManagementRepository.java
@@ -2,8 +2,12 @@ package io.yadnyesh.kksball.repository;
 
 import io.yadnyesh.kksball.entity.Person;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.scheduling.annotation.Async;
 
+import java.util.concurrent.CompletableFuture;
 
 public interface PersonManagementRepository extends CrudRepository<Person, Integer> {
-
+	
+	@Async
+	CompletableFuture<Person> findByEmail(String email);
 }
