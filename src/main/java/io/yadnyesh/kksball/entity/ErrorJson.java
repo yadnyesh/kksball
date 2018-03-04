d2ed6c5356918f6a4b5ad3dc04220e39da4bfcfa
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-03-04 10:31:13 +0530

    Added constructor and getter and setter methods

--- src/main/java/io/yadnyesh/kksball/entity/ErrorJson.java
+++ src/main/java/io/yadnyesh/kksball/entity/ErrorJson.java
@@ -7,5 +7,51 @@ public class ErrorJson {
 	private String timeStamp;
 	private String trace;
 	
+	public ErrorJson(Integer status, String error, String message, String timeStamp, String trace) {
+		this.status = status;
+		this.error = error;
+		this.message = message;
+		this.timeStamp = timeStamp;
+		this.trace = trace;
+	}
 	
+	public Integer getStatus() {
+		return status;
+	}
+	
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+	
+	public String getError() {
+		return error;
+	}
+	
+	public void setError(String error) {
+		this.error = error;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public String getTimeStamp() {
+		return timeStamp;
+	}
+	
+	public void setTimeStamp(String timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+	
+	public String getTrace() {
+		return trace;
+	}
+	
+	public void setTrace(String trace) {
+		this.trace = trace;
+	}
 }
