a65b736d8a5e45df221807db8cbc7906f18e8104
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-03-04 10:54:43 +0530

    Changes to get Error in JSON format

--- src/main/java/io/yadnyesh/kksball/controller/SampleController.java
+++ src/main/java/io/yadnyesh/kksball/controller/SampleController.java
@@ -1,10 +1,18 @@
 package io.yadnyesh.kksball.controller;
 
+import io.yadnyesh.kksball.entity.ErrorJson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.ErrorAttributes;
 import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @RestController
 public class SampleController implements ErrorController {
@@ -30,5 +38,12 @@ public class SampleController implements ErrorController {
 //	}
 
 	@GetMapping(PATH)
-	ErrorJson
+	ErrorJson error(HttpServletRequest request, HttpServletResponse response) {
+		return new ErrorJson(response.getStatus(), getErrorAttributes(request, true));
+	}
+	
+	private Map<String, Object> getErrorAttributes(HttpServletRequest request, boolean includeStackTrace) {
+		RequestAttributes requestAttributes = new ServletRequestAttributes(request);
+		return errorAttributes.getErrorAttributes(requestAttributes, includeStackTrace);
+	}
 }
--- src/main/java/io/yadnyesh/kksball/entity/ErrorJson.java
+++ src/main/java/io/yadnyesh/kksball/entity/ErrorJson.java
@@ -1,5 +1,7 @@
 package io.yadnyesh.kksball.entity;
 
+import java.util.Map;
+
 public class ErrorJson {
 	private Integer status;
 	private String error;
@@ -7,12 +9,20 @@ public class ErrorJson {
 	private String timeStamp;
 	private String trace;
 	
-	public ErrorJson(Integer status, String error, String message, String timeStamp, String trace) {
-		this.status = status;
+	
+	public ErrorJson(String error, String message, String timeStamp, String trace) {
 		this.error = error;
 		this.message = message;
 		this.timeStamp = timeStamp;
 		this.trace = trace;
+	}
+	
+	public ErrorJson(Integer status, Map<String, Object> errorAttributes) {
+		this.status = status;
+		this.error = (String) errorAttributes.get("error");
+		this.message = (String) errorAttributes.get("message");
+		this.timeStamp = errorAttributes.get("timestamp").toString();
+		this.trace = (String) errorAttributes.get("trace");
 	}
 	
 	public Integer getStatus() {
