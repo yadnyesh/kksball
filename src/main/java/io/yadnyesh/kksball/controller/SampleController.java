e39642354826befb13aeb1ea59c0bae8604ab5a1
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-03-04 10:30:27 +0530

    Defining fields in ErrorJson

--- src/main/java/io/yadnyesh/kksball/controller/SampleController.java
+++ src/main/java/io/yadnyesh/kksball/controller/SampleController.java
@@ -1,11 +1,16 @@
 package io.yadnyesh.kksball.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ErrorAttributes;
 import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleController implements ErrorController {
+	
+	@Autowired
+	ErrorAttributes errorAttributes;
 	
 	private static final String PATH="/error";
 	
@@ -19,8 +24,11 @@ public class SampleController implements ErrorController {
 		return "Welcome, Yadnyesh!";
 	}
 	
+//	@GetMapping(PATH)
+//	public String displayError(){
+//		return "Displaying custom Error!!!";
+//	}
+
 	@GetMapping(PATH)
-	public String displayError(){
-		return "Displaying custom Error!!!";
-	}
+	ErrorJson
 }
--- src/main/java/io/yadnyesh/kksball/entity/ErrorJson.java
+++ src/main/java/io/yadnyesh/kksball/entity/ErrorJson.java
@@ -1,3 +1,11 @@
 package io.yadnyesh.kksball.entity;
 
-public class ErrorJson {}
+public class ErrorJson {
+	private Integer status;
+	private String error;
+	private String message;
+	private String timeStamp;
+	private String trace;
+	
+	
+}
