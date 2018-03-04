fddef48a49c2dc95a91730729661218f93918a42
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-03-04 08:43:10 +0530

    Relevant changes to Controller class

--- src/main/java/io/yadnyesh/kksball/controller/SampleController.java
+++ src/main/java/io/yadnyesh/kksball/controller/SampleController.java
@@ -1,3 +1,13 @@
 package io.yadnyesh.kksball.controller;
 
-public class SampleController {}
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+	
+	@GetMapping("/")
+	public String welcome(){
+		return "Welcome, Yadnyesh!";
+	}
+}
