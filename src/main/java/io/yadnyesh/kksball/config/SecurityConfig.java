1238b906785e596eea0a0a8e782a797d9dd172bb
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-03-16 02:35:32 +0530

    Changes for in-memory authentication

--- src/main/java/io/yadnyesh/kksball/config/SecurityConfig.java
+++ src/main/java/io/yadnyesh/kksball/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package io.yadnyesh.kksball.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,7 +13,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity.csrf().disable().authorizeRequests().antMatchers("/api/*");
+		httpSecurity.csrf().disable().authorizeRequests().antMatchers("/api/*")
+				.hasAnyRole("admin", "user").and().formLogin();
 	}
 
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("sean").roles("user");
+		auth.inMemoryAuthentication().withUser("test").roles("test");
+	}
 }
