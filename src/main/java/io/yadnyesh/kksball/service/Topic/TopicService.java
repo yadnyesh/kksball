05e01a88d02603295b8fa3a67b172181ba52d1f1
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-03-17 01:35:39 +0530

    Securing TopicService Methods

--- src/main/java/io/yadnyesh/kksball/service/Topic/TopicService.java
+++ src/main/java/io/yadnyesh/kksball/service/Topic/TopicService.java
@@ -5,6 +5,7 @@ import io.yadnyesh.kksball.repository.Topic.TopicRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,15 +18,18 @@ public class TopicService {
 	@Autowired
 	TopicRepository topicRepository;
 	
+	@Secured({"ROLE_ADMIN", "ROLE_USER"})
 	public Topic getTopicById(int topicId){
 		Topic topic = topicRepository.getTopicById(topicId);
 		return topic;
 	}
 	
+	@Secured({"ROLE_ADMIN", "ROLE_USER"})
 	public List<Topic> getAllTopics() {
 		return topicRepository.getAllTopics();
 	}
 	
+	@Secured({"ROLE_ADMIN"})
 	public boolean addTopic(Topic topic) {
 		if(topicRepository.topicExists(topic.getTitle(), topic.getCategory())) {
 			LOGGER.error("Topic {0} already exists.", topic.getTitle());
@@ -37,10 +41,12 @@ public class TopicService {
 		}
 	}
 	
+	@Secured({"ROLE_ADMIN"})
 	public void updateTopic(Topic topic) {
 		topicRepository.updateTopic(topic);
 	}
 	
+	@Secured({"ROLE_ADMIN"})
 	public void deleteTopic(int topicId){
 		topicRepository.deleteTopic(topicId);
 	}
