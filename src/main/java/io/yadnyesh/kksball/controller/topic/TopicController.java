5047bf724e6d1e04b7b0b404e5fc0d875b9cbe5b
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-03-16 22:53:49 +0530

    Added Topic Entity

--- src/main/java/io/yadnyesh/kksball/controller/topic/TopicController.java
+++ src/main/java/io/yadnyesh/kksball/controller/topic/TopicController.java
@@ -1,7 +1,9 @@
 package io.yadnyesh.kksball.controller.topic;
 
+import io.yadnyesh.kksball.entity.Topic.Topic;
 import io.yadnyesh.kksball.service.Topic.TopicService;
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class TopicController {
 	
+	@Autowired
+	public TopicController(TopicService topicService) {}
 	
-	public TopicController(TopicService topicService) {
-	}
+	public ResponseEntity<Topic>
 }
--- src/main/java/io/yadnyesh/kksball/entity/Topic/Topic.java
+++ src/main/java/io/yadnyesh/kksball/entity/Topic/Topic.java
@@ -1,5 +1,45 @@
 package io.yadnyesh.kksball.entity.Topic;
 
-public class Topic {
+import javax.persistence.*;
 
+@Entity
+@Table(name = "topics")
+public class Topic {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="topic_id")
+	private int topicId;
+	
+	@Column(name="title", length = 100, nullable = false)
+	private String title;
+	
+	@Column(name="category", length = 100, nullable = false)
+	private String category;
+	
+	public int getTopicId() {
+		return topicId;
+	}
+	
+	public void setTopicId(int topicId) {
+		this.topicId = topicId;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getCategory() {
+		return category;
+	}
+	
+	public void setCategory(String category) {
+		this.category = category;
+	}
 }
