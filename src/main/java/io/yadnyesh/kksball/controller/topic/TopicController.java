85d2c900ba98576950438be19490786c6745622d
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-03-16 22:47:30 +0530

    Added to Topic Controller class

--- src/main/java/io/yadnyesh/kksball/controller/topic/TopicController.java
+++ src/main/java/io/yadnyesh/kksball/controller/topic/TopicController.java
@@ -1,3 +1,11 @@
 package io.yadnyesh.kksball.controller.topic;
 
-public class TopicController {}
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class TopicController {
+
+}
