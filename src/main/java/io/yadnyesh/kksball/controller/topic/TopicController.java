d3674c78033c6214a825e4abf38c7be6befe1c12
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-03-17 00:06:55 +0530

    Added getTopicById method

--- src/main/java/io/yadnyesh/kksball/controller/topic/TopicController.java
+++ src/main/java/io/yadnyesh/kksball/controller/topic/TopicController.java
@@ -3,7 +3,9 @@ package io.yadnyesh.kksball.controller.topic;
 import io.yadnyesh.kksball.entity.Topic.Topic;
 import io.yadnyesh.kksball.service.Topic.TopicService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +19,9 @@ public class TopicController {
 	@Autowired
 	public TopicController(TopicService topicService) {}
 	
+	@GetMapping("/topic/{id}")
 	public ResponseEntity<Topic> getTopicById(@PathVariable ("id") Integer id) {
-		Topic topic = topicService
+		Topic topic = topicService.getTopicById(id);
+		return new ResponseEntity<Topic>(topic, HttpStatus.OK);
 	}
 }
