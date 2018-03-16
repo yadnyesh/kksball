e79f1184143426f6945d133f4143d384241e55d2
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-03-16 23:44:56 +0530

    Changes for TopicService

--- src/main/java/io/yadnyesh/kksball/service/Topic/TopicService.java
+++ src/main/java/io/yadnyesh/kksball/service/Topic/TopicService.java
@@ -1,8 +1,19 @@
 package io.yadnyesh.kksball.service.Topic;
 
+import io.yadnyesh.kksball.entity.Topic.Topic;
+import io.yadnyesh.kksball.repository.Topic.TopicRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
 
+	@Autowired
+	TopicRepository topicRepository;
+	
+	public Topic getTopicById(int topicId){
+		Topic topic = topicRepository.getTopicById(topicId);
+		return topic;
+	}
+	
 }
