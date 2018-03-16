a033d01fce3981e71f7c8e385b4511721ad71a4d
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-03-16 22:58:37 +0530

    Created TopicRepository interface

--- src/main/java/io/yadnyesh/kksball/controller/topic/TopicController.java
+++ src/main/java/io/yadnyesh/kksball/controller/topic/TopicController.java
@@ -4,6 +4,7 @@ import io.yadnyesh.kksball.entity.Topic.Topic;
 import io.yadnyesh.kksball.service.Topic.TopicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class TopicController {
 	
+	TopicService topicService;
+	
 	@Autowired
 	public TopicController(TopicService topicService) {}
 	
-	public ResponseEntity<Topic>
+	public ResponseEntity<Topic> getTopicById(@PathVariable ("id") Integer id) {
+		Topic topic = topicService.get
+	}
 }
--- src/main/java/io/yadnyesh/kksball/repository/Topic/TopicRepository.java
+++ src/main/java/io/yadnyesh/kksball/repository/Topic/TopicRepository.java
@@ -1,3 +1,14 @@
 package io.yadnyesh.kksball.repository.Topic;
 
-public class TopicRepository {}
+import io.yadnyesh.kksball.entity.Topic.Topic;
+
+import java.util.List;
+
+public interface TopicRepository {
+	public abstract List<Topic> getAllTopics();
+	public abstract Topic getTopicById(int topicId);
+	public abstract void addTopic(Topic topic);
+	public abstract void updateTopic(Topic topic);
+	public abstract void deleteTopic(int topicId);
+	public abstract boolean topicExists(String title, String Category);
+}
