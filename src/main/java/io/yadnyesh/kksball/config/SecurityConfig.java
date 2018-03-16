8cf064756acd35163571e38f33ccead5a7b006c5
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-03-17 01:15:15 +0530

    Created UserDetailsService

--- src/main/java/io/yadnyesh/kksball/config/SecurityConfig.java
+++ src/main/java/io/yadnyesh/kksball/config/SecurityConfig.java
@@ -3,12 +3,13 @@ package io.yadnyesh.kksball.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
---
+++ src/main/java/io/yadnyesh/kksball/service/Topic/UserDetailsService.java
@@ -0,0 +1,8 @@
+package io.yadnyesh.kksball.service.Topic;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsService {
+
+}
