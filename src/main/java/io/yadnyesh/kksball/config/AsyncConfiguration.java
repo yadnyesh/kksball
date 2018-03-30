5604e4ed9f8fa7fe23b483ef3a24bab20e8f0ccb
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-03-31 00:33:37 +0530

    Created AsyncConfiguration

--- src/main/java/io/yadnyesh/kksball/config/AsyncConfiguration.java
+++ src/main/java/io/yadnyesh/kksball/config/AsyncConfiguration.java
@@ -1,6 +1,25 @@
 package io.yadnyesh.kksball.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-public class AsyncConfiguration {
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfiguration extends AsyncConfigurerSupport {
+	
+	@Override
+	public Executor getAsyncExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(10);
+		executor.setMaxPoolSize(20);
+		executor.setQueueCapacity(20);
+		executor.setThreadNamePrefix("AsyncConfigurerSupportDemo");
+		executor.initialize();
+		return executor;
+	}
 
 }
