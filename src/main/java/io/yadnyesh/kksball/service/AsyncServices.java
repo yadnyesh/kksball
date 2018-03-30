b568996e9c0110240ed011dfd3ab93d25f79f5a3
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-03-31 00:25:34 +0530

    Completed changes to AsyncServices

--- src/main/java/io/yadnyesh/kksball/service/AsyncServices.java
+++ src/main/java/io/yadnyesh/kksball/service/AsyncServices.java
@@ -2,10 +2,22 @@ package io.yadnyesh.kksball.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.Future;
 
 @Service
 public class AsyncServices {
 	Logger logger = LoggerFactory.getLogger(AsyncServices.class);
+	
+	@Async
+	public Future<String> process() throws InterruptedException {
+		logger.info("###Start Processing with Thread id: " + Thread.currentThread().getId());
+		Thread.sleep(2000);
+		String processInfo = String.format("Processing is Done with Thread id= %d", Thread.currentThread().getId());
+		return new AsyncResult<>(processInfo);
+	}
 	
 }
