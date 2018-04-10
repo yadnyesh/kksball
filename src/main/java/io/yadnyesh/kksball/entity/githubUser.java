c4b83bff7437aa2afbee616cb05e2d758d2f60a6
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-04-10 14:22:40 +0530

    Created githubUser Class

--- src/main/java/io/yadnyesh/kksball/entity/githubUser.java
+++ src/main/java/io/yadnyesh/kksball/entity/githubUser.java
@@ -5,4 +5,27 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class githubUser {
 
+	private String user;
+	private String blog;
+	
+	public String getUser() {
+		return user;
+	}
+	
+	public void setUser(String user) {
+		this.user = user;
+	}
+	
+	public String getBlog() {
+		return blog;
+	}
+	
+	public void setBlog(String blog) {
+		this.blog = blog;
+	}
+	
+	@Override
+	public String toString() {
+		return "githubUser{" + "user='" + user + '\'' + ", blog='" + blog + '\'' + '}';
+	}
 }
