9a8da711355be4d814effb3387338ec7c17293a7
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-04-11 09:30:12 +0530

    Constructor injection of RestTemplate

--- src/main/java/io/yadnyesh/kksball/service/GithubLookupService.java
+++ src/main/java/io/yadnyesh/kksball/service/GithubLookupService.java
@@ -1,8 +1,19 @@
 package io.yadnyesh.kksball.service;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import org.slf4j.Logger;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class GithubLookupService {
-
+	private static final Logger logger = LoggerFactory.getLogger(GithubLookupService.class);
+	private RestTemplate restTemplate;
+	
+	public GithubLookupService(RestTemplate restTemplate){
+		this.restTemplate = restTemplate;
+	}
+	
+	
 }
