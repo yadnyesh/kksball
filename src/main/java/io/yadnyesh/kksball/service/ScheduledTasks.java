4c76c4f2ce8b092615929bfdc4f656be82a9f67e
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-05-19 08:46:25 -0500

    Disabling scheduled tasks

--- src/main/java/io/yadnyesh/kksball/service/ScheduledTasks.java
+++ src/main/java/io/yadnyesh/kksball/service/ScheduledTasks.java
@@ -13,7 +13,7 @@ public class ScheduledTasks {
 	private static final Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
 	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 	
-	@Scheduled(fixedRate = 50000)
+//	@Scheduled(fixedRate = 50000)
 	public void reportCurrentTime() {
 		logger.info("The time now is {}", dateFormat.format(new Date()) );
 	}
