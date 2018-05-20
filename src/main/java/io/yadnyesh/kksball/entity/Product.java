467b8725f595d5a2ea388a8bb646ee19b9b3d797
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-05-20 18:18:58 -0500

    Creating domain for Spring cache

---
+++ src/main/java/io/yadnyesh/kksball/entity/Product.java
@@ -0,0 +1,33 @@
+package io.yadnyesh.kksball.entity;
+
+import java.io.Serializable;
+
+public class Product implements Serializable {
+	private String id;
+	private String ean;
+	private String name;
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getEan() {
+		return ean;
+	}
+	
+	public void setEan(String ean) {
+		this.ean = ean;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+}
