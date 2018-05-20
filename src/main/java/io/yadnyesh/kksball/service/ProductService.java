d7624ef3a56717ce04cd44cee034d6c866e51790
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-05-20 18:20:37 -0500

    Added Service Class and Logger

---
+++ src/main/java/io/yadnyesh/kksball/service/ProductService.java
@@ -0,0 +1,10 @@
+package io.yadnyesh.kksball.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ProductService {
+	private static final Logger LOGGER = LoggerFactory.getLogger(ProductService.class);
+	
+	
+}
