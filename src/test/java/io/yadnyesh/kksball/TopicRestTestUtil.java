9d7ba65b18c81bae60a590003c83f1efd61cdd4b
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-06-01 05:03:01 +0530

    Part 64 - Configure Headers to be used in RestTemplate

--- src/test/java/io/yadnyesh/kksball/TopicRestTestUtil.java
+++ src/test/java/io/yadnyesh/kksball/TopicRestTestUtil.java
@@ -1,5 +1,19 @@
 package io.yadnyesh.kksball;
 
-public class TopicRestTestUtil {
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 
+public class TopicRestTestUtil {
+	
+		private HttpHeaders getHeaders() {
+			String credential = "s@123";
+			String encodedCredential = new String(Base64.encodeBase64(credential.getBytes()));
+			HttpHeaders headers = new HttpHeaders();
+			headers.setContentType(MediaType.APPLICATION_JSON);
+			headers.add("Authorization", "Basic " + encodedCredential);
+			return headers;
+		}
+		
+		
 }
