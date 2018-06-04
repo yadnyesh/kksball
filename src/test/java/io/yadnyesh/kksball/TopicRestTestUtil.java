c6e305948f618c877e0ad7113a5a7ab0146f032c
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-06-04 16:04:39 +0530

    Part 64 - Update topic Demo

--- src/test/java/io/yadnyesh/kksball/TopicRestTestUtil.java
+++ src/test/java/io/yadnyesh/kksball/TopicRestTestUtil.java
@@ -52,8 +52,8 @@ public class TopicRestTestUtil {
 		topic.setTopicId(1);
 		topic.setTitle("Updated: Java Get upto speed");
 		topic.setCategory("Java");
-		HttpEntity<Topic> httpEntity = new HttpEntity<Topic>(topic, headers);
-		restTemplate.put(url, headers);
+		HttpEntity<Topic> requestEntity = new HttpEntity<Topic>(topic, headers);
+		restTemplate.put(url, requestEntity);
 	}
 	
 	private HttpHeaders getHeaders() {
@@ -69,7 +69,7 @@ public class TopicRestTestUtil {
 		TopicRestTestUtil topicRestTestUtil = new TopicRestTestUtil();
 		topicRestTestUtil.getTopicByIdDemo();
 		topicRestTestUtil.getAllTopicsDemo();
-		topicRestTestUtil.addTopicDemo();
+		//topicRestTestUtil.addTopicDemo();
 		topicRestTestUtil.updateTopicDemo();
 	}
 }
