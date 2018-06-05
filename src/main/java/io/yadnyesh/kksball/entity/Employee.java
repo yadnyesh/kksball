a1c9a327cfb286d5bbfa5ebff21649f31867805f
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-06-05 13:42:57 +0530

    Part 68 - Spring JPA - Created Employee Entity

---
+++ src/main/java/io/yadnyesh/kksball/entity/Employee.java
@@ -0,0 +1,79 @@
+package io.yadnyesh.kksball.entity;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name="employee_table")
+public class Employee {
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="employee_id")
+	private Integer employeeId;
+	
+	@Column(name="employee_name")
+	private String employeeName;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="salary")
+	private Double salary;
+	
+	public Integer getEmployeeId() {
+		return employeeId;
+	}
+	
+	public void setEmployeeId(Integer employeeId) {
+		this.employeeId = employeeId;
+	}
+	
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public Double getSalary() {
+		return salary;
+	}
+	
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if(this == o) {
+			return true;
+		}
+		if(o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		Employee employee = (Employee) o;
+		return Objects.equals(employeeId, employee.employeeId) && Objects.equals(employeeName, employee.employeeName) && Objects
+				.equals(email, employee.email) && Objects.equals(salary, employee.salary);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(employeeId, employeeName, email, salary);
+	}
+	
+	@Override
+	public String toString() {
+		return "Employee{" + "employeeId=" + employeeId + ", employeeName='" + employeeName + '\'' + ", email='" + email + '\'' + ", salary=" + salary + '}';
+	}
+}
