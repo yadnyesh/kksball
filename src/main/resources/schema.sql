c4bf875f5f41b18e9f86797ce5b77d2e83bf458a
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-06-06 10:29:17 +0530

    Part 68 - Changes for SonarQube - working now

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,19 +1,30 @@
 drop table if exists topics;
 drop table if exists users;
+drop table if exists employee_table;
 
 create table topics (
-        topic_id SERIAL,
-        category varchar(100) not null,
-        title varchar(100) not null,
-        primary key (topic_id)
-    );
+    topic_id SERIAL,
+    category varchar(100) not null,
+    title varchar(100) not null,
+    primary key (topic_id)
+);
 
-     create table users (
-        username varchar(50) not null,
-        country varchar(30),
-        enabled smallint,
-        full_name varchar(100),
-        password varchar(800) not null,
-        role varchar(50),
-        primary key (username)
+ create table users (
+    username varchar(50) not null,
+    country varchar(30),
+    enabled smallint,
+    full_name varchar(100),
+    password varchar(800) not null,
+    role varchar(50),
+    primary key (username)
+);
+
+
+drop table if exists employee_table;
+ create table employee_table (
+        employee_id SERIAL,
+        email varchar(255),
+        employee_name varchar(255),
+        salary double precision,
+        primary key (employee_id)
     );
