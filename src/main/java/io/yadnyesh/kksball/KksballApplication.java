fe00a42f995675b0af257ac794b89e4ba02f35f9
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-06-07 10:39:36 +0530

    Lecture 69 - Working code Spring Boot JPA with Hibernate

--- src/main/java/io/yadnyesh/kksball/KksballApplication.java
+++ src/main/java/io/yadnyesh/kksball/KksballApplication.java
@@ -13,15 +13,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.Arrays;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Future;
 import java.util.stream.Collectors;
 
 @SpringBootApplication
@@ -29,16 +24,8 @@ import java.util.stream.Collectors;
 @EnableScheduling
 public class KksballApplication implements CommandLineRunner {
 	
-	private static final Logger LOGGER = LoggerFactory.getLogger(KksballApplication.class);
-	
 	@Autowired
 	AsyncService asyncService;
-	
-	@Autowired
-	private PersonManagementService personManagementService;
-	
-	@Autowired
-	private GithubLookupService githubLookupService;
 	
 	@Autowired
 	EnvBasedConfig envBasedConfig;
@@ -53,7 +40,7 @@ public class KksballApplication implements CommandLineRunner {
 	}
 	
 	@Override
-	public void run(String... args) throws Exception {
+	public void run(String... args) {
 		envBasedConfig.setup();
 		String collectStr = Arrays.stream(args).collect(Collectors.joining(","));
 		System.out.println(collectStr);
